d496536ca34e832dc60dd24e52749e729f1097ed
GordonOtieno <otienogordon95@gmail.com>
2022-07-18 07:23:02 -0400

    add javascript file

---
+++ script.js
@@ -0,0 +1,9 @@
+
+hamburger.addEventListener('click', ()=>{
+    navUl.classList.toggle('show');
+
+})
+function toggleNav(){
+    const navUl=document.getElementById('nav-link');
+    navUl.classList.toggle('show');
+}
