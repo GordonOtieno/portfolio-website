91d8196aa9ecbe44993f1a9638ba47823bf0ac13
enning94 <79149176+Enning94@users.noreply.github.com>
2022-07-19 09:17:53 +0000

    Fix linters error

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function toggleNav(){
-    const navUl=document.getElementById('nav-link');
-    const mainId=document.getElementById('main-logo');
-    const navrev=document.getElementById('reversed');
-    const close_hum=document.getElementById('close-hum');
-    navUl.classList.toggle('show');
-    mainId.classList.toggle('hide');
-    navrev.classList.toggle('reversed');
-    close_hum.classList.toggle('hide');
-
+function toggleNav() {
+  const navUl = document.getElementById("nav-link");
+  const mainId = document.getElementById("main-logo");
+  const navrev = document.getElementById("reversed");
+  const close_hum = document.getElementById("close-hum");
+  navUl.classList.toggle("show");
+  mainId.classList.toggle("hide");
+  navrev.classList.toggle("reversed");
+  close_hum.classList.toggle("hide");
 }
