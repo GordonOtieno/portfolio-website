47fa665fe9f95a709441acf11d5d82611a874e53
enning94 <79149176+Enning94@users.noreply.github.com>
2022-07-19 09:01:18 +0000

    Add js features to control hamburger menu

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-
-hamburger.addEventListener('click', ()=>{
-    navUl.classList.toggle('show');
-
-})
 function toggleNav(){
     const navUl=document.getElementById('nav-link');
+    const mainId=document.getElementById('main-logo');
+    const navrev=document.getElementById('reversed');
+    const close_hum=document.getElementById('close-hum');
     navUl.classList.toggle('show');
+    mainId.classList.toggle('hide');
+    navrev.classList.toggle('reversed');
+    close_hum.classList.toggle('hide');
+
 }
